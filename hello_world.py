5159829ab971cd241733795d86ce9c02305ff380
tappikone <testnet.expert@gmail.com>
2023-01-17 18:52:31 +0200

    Add prints

--- hello_world.py
+++ hello_world.py
@@ -1,28 +1,10 @@
 print("Hello world!")
 print("Not another hello world")
-<<<<<<< HEAD
-<<<<<<< HEAD
 print("No")
 print("More")
-<<<<<<< HEAD
-<<<<<<< HEAD
 print("Hello")
 print("World")
-<<<<<<< HEAD
-<<<<<<< HEAD
 print("Please, I'm begging you")
-=======
->>>>>>> fbb9ca9 (asd)
-=======
 print("No")
->>>>>>> 94c4fbf (Line change)
-=======
->>>>>>> 51905dc (test2)
-=======
 print("Hello")
->>>>>>> 1662e16 (commit)
-=======
->>>>>>> c3cf8d5 (qwerty)
-=======
 print("Please, I'm begging you")
->>>>>>> ca63dde (fix typo)
